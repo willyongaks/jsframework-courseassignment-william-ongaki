c941135253dfe1d3ca4330fd6d64d2fcf22ef8ef
willyongaks <83573335+willyongaks@users.noreply.github.com>
2022-12-15 10:11:02 +0100

    n

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -17,7 +17,7 @@ function Layout() {
     return (
         <Router>
             <Navbar bg="dark" variant='dark' expand="lg">
-                <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand to="/">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
--- src/components/post/Postlist.js
+++ src/components/post/Postlist.js
@@ -37,7 +37,11 @@ function Postlist() {
 
   return (
     <div>
-      {post.map(post => <div key={post.id}>{post.title.rendered}</div>)}
+      {post.map(post => (
+      <div key={post.id}>
+        <h2>{post.title.rendered}</h2>
+          <img src={post.better_featured_image.source_url} alt={post.better_featured_image.alt_text} />
+      </div>))}
     </div>
   );
 }
