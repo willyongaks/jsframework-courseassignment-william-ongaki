e834d060125bc9cb32d8b7246ae7fdb74c9c046b
willyongaks <83573335+willyongaks@users.noreply.github.com>
2022-12-15 12:04:37 +0100

    n

--- src/components/post/Postlist.js
+++ src/components/post/Postlist.js
@@ -6,6 +6,13 @@ function Postlist() {
   const [loading, setLoading] = useState();
   const [error, setError] = useState();
 
+   // stylesheet
+  // const styles = {
+  //   image: {
+  //     width:"100%"
+  //   }
+  // }
+
   useEffect(() => {
     async function getPost() {
       try {
@@ -36,11 +43,11 @@ function Postlist() {
   }
 
   return (
-    <div>
+    <div className="post-container">
       {post.map(post => (
-      <div key={post.id}>
-        <h2>{post.title.rendered}</h2>
-          <img src={post.better_featured_image.source_url} alt={post.better_featured_image.alt_text} />
+        <div key={post.id} className="post-card">
+        <h2 className='post-title'>{post.title.rendered}</h2>
+          <img src={post.better_featured_image.source_url} alt={post.better_featured_image.alt_text} className="post-image" />
       </div>))}
     </div>
   );
