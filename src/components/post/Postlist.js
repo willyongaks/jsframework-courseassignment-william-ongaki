6b924b6b3c1a7becc4966bf2b7d8ee430b5c4383
willyongaks <83573335+willyongaks@users.noreply.github.com>
2022-12-15 09:48:57 +0100

    n

--- src/components/post/Postlist.js
+++ src/components/post/Postlist.js
@@ -1,48 +1,42 @@
 import { API } from '../../constants/api';
 import { useState, useEffect } from 'react'
-import { json } from 'react-router-dom';
 
 function Postlist() {
-    const [post, setPost] = useState([]);
-    const [loading, setLoading] = useState([true]);
-    const [error, setError] = useState([null]);
+  const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState();
 
-    useEffect(() => {
-        async function getPost(){
-          try {
-            const response = await fetch(API);
+  useEffect(() => {
+    async function getPost() {
+      try {
+        const response = await fetch(API);
 
-            if(response.ok){
-              const results = await response.json();
-              console.log(results);
-              setPost(json) 
-            } else {
-              setError("An error occured")
-            }
-          }catch(error){
-            console.log(error)
-          }finally{
-            setLoading(false)
-          }
+        if (response.ok) {
+          const results = await response.json();
+          setPost(results);
+        } else {
+          setError("An error occured");
         }
-        getPost()
-    },[]);
-
-    if(loading){
-      return <div>Loading....</div>
+      } catch (error) {
+        console.log(error);
+      }
     }
+    getPost();
+  }, []);
+
+  if (loading) {
+    return <div>Loading....</div>
+  }
 
   if (error) {
-    return <div>Error: An error occured....</div>
+    return <div>{error}</div>
   }
 
   return (
-    <>
-      {post.map(function(post){
-        return <div key={post.id}>{post.slug}</div>
-      })}
-    </>
-  )
+    <div>
+      {post.map(post => <div key={post.id}>`&{post.slug}`</div>)}
+    </div>
+  );
 }
 
 export default Postlist;
